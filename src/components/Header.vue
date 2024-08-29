78ca2ee7a398375fd24a2232d348e57284563cd1
pierobramanti <piero.bramanti94@gmail.com>
2024-08-29 14:22:55 +0200

    [FIX] fixed App.vue and changed name components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,13 @@
 <script>
 export default {
-
+    
 }
 </script>
 
 <template>
-    <div>Footer</div>
+    <div>Header</div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,11 @@
 <script>
+    export default {
+
+    }
 </script>
 
 <template>
-
+    <div>Main</div>
 </template>
 
 <style lang="scss" scoped>
