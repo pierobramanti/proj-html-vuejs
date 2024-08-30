83db461bd7635635f184a601bc6aad6588355766
pierobramanti <piero.bramanti94@gmail.com>
2024-08-30 13:08:11 +0200

    [UPD] working on header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,16 @@
 <script>
 export default {
-    
+    data() {
+        return {
+            menuItems: [
+                { label: "Home", link: "#", isActive: true },
+                { label: "About", link: "#", isActive: false },
+                { label: "Services", link: "#", isActive: false },
+                { label: "work", link: "#", isActive: false },
+                { label: "articles", link: "#", isActive: false }
+            ]
+        }
+    }
 }
 </script>
 
@@ -11,12 +21,10 @@ export default {
                 <div class="d-flex justify-content-between align-items-center px-4 py-4">
                     <img class="img-fluid border" src="../assets/img/construction_logo.png" alt="Avada construction Logo">
                     <ul class="list-unstyled d-flex align-items-center">
-                        <li class="px-4 "><a class="text-decoration-none text-dark text-uppercase" href="#">Home</a></li>
-                        <li class="px-4 "><a class="text-decoration-none text-dark text-uppercase" href="#">About</a></li>
-                        <li class="px-4 "><a class="text-decoration-none text-dark text-uppercase" href="#">Services</a></li>
-                        <li class="px-4 "><a class="text-decoration-none text-dark text-uppercase" href="#">Work</a></li>
-                        <li class="px-4 "><a class="text-decoration-none text-dark text-uppercase" href="#">Articles</a></li>
-                        <li><button class="px-4 text-uppercase btn" href="#">Get Quote</button></li>
+                        <li v-for="item in menuItems" :key="item.label" class="px-4" :class="{ 'active-class': item.isActive }">
+                            <a class="text-decoration-none text-dark text-uppercase" :href="item.link">{{ item.label }}</a>
+                        </li>
+                        <li><button class="px-4 text-uppercase" href="#">Get Quote</button></li>
                     </ul>
                 </div>
             </div>
@@ -27,13 +35,20 @@ export default {
 <style lang="scss" scoped>
     @use "../components/style/partials/vars" as *;
 
-    li:hover{
-        color:  $yellow ;
+    li:hover a {
+        color: $yellow !important;
     }
 
     button {
         background-color: $yellow;
         padding: 5px 40px;
-      }
+        border: none;
+        outline: none;
+
+        &:hover {
+            background-color: $light_black;
+            color: $grey;
+        }
+    }
 
 </style>
