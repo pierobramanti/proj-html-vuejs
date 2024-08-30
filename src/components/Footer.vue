025c5689b1936c0a0c0ed480ee756e8ab3469499
pierobramanti <piero.bramanti94@gmail.com>
2024-08-30 14:39:04 +0200

    [UPD] installed fontawesome and working on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,9 +5,60 @@ export default {
 </script>
 
 <template>
-    <div>Footer</div>
+    <div>
+        <div class="container-full">
+            <div class="row">
+                <div class="col-12">
+                    <div class="col-4">
+
+                    </div>
+                    <div class="col-4">
+                        
+                    </div>
+                    <div class="col-4">
+                        
+                    </div>
+                    <div class="col-4">
+                        
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <div class="container-full">
+            <div class="row border">
+                <div class="col-12 px-4">
+                    <div class="py-4 d-flex justify-content-between">
+                        <div>
+                            <h5 class="text-uppercase">copy-right 2012-2015 avada theme by theme fusion | all right reserved | powered by wordpress</h5>
+                        </div>
+                        <div class="d-flex">
+                            <a class="mx-2" href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                            <a class="mx-2" href="#"><i class="fa-brands fa-twitter"></i></a>
+                            <a class="mx-2" href="#"><i class="fa-brands fa-youtube"></i></a>
+                            <a class="mx-2" href="#"><i class="fa-brands fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+@use "../components/style/partials/vars" as *;
+
+.container-full:nth-child(2) {
+    background-color: $black;
+}
+
+h5 {
+    font-size: 12px;
+    color: $light_black;
+}
+
+a{
+    color:$light_black;
+}
 
 </style>
