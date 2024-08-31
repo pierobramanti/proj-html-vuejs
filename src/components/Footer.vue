bdef9dd41ed30a93b1fd97c9505994d647e2e6fb
pierobramanti <piero.bramanti94@gmail.com>
2024-08-31 19:09:26 +0200

    [UPD] working on statics layout footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,29 +5,29 @@ export default {
 </script>
 
 <template>
-    <div>
-        <div class="container-full">
+    <div class="footer bg-color-light-black">
+        <div class="container">
             <div class="row">
-                <div class="col-12 d-flex px-4 py-4">
-                    <div class="learn-more col-3 border bg-color">
-                        <img src="../assets/img/footer-logo.png" alt="">
-                        <p class="lh-lg"> We are specialist in the costruction of unique and exclusive properties. Our work inspires. We pride ourselves on delivering outstanding quality and design for leading clients across the world.</p>
+                <div class="col-12 d-flex px-2 py-4">
+                    <div class="learn-more col-3 me-2">
+                        <img class="mb-4" src="../assets/img/footer-logo.png" alt="">
+                        <p class="lh-lg fonts-11 text-secondary"> We are specialist in the costruction of unique and exclusive properties. Our work inspires. We pride ourselves on delivering outstanding quality and design for leading clients across the world.</p>
                         <button class="px-4 text-uppercase" href="#">Learn more</button>
                     </div>
-                    <div class="col-3 tweets border">
-                        <h4>Latest tweets</h4>
+                    <div class="col-3 tweets me-2">
+                        <h6 class="mb-3 fw-bold">Latest Tweets</h6>
                         <div class="col-12 d-flex">
                             <div class="col-2">
-                                <img class="img-fluid" src="../assets/img/3a74ce3d0532b7773b174c45ca3bd05a_normal.png" alt="">
+                                <img class="img-fluid w-50 rounded-circle" src="../assets/img/3a74ce3d0532b7773b174c45ca3bd05a_normal.png" alt="">
                             </div>
                             <div class="col-10">
                                <div class="d-flex justify-content-between">
-                                <h4 class="fw-bold fs-6">ThemeFusion</h4>
+                                <h4 class="fw-bold fonts-10">ThemeFusion</h4>
                                 <a class="mx-2" href="#"><i class="fa-brands fa-twitter"></i></a>
                                </div>
                                <div>
-                                <h6>@Theme_Fusion</h6>
-                                <p class="fonts-9 lh-sm fw-bolder "> Do you need sublime WordPress hosting for your next website? Take advantage of exclusive parteners offers that we have secured just for you, & launch your site in seconds with <a href=""> #avada</a> on WP Engine hosting & get 30% Off this Black Friday <a href=""> bit.ly/3kjLLE2 #BlackFriday2020</a></p>
+                                <p class="fw-light fonts-10 text-white ">@Theme_Fusion</p>
+                                <p class="fonts-9 lh-sm fw-bolder text-white"> Do you need sublime WordPress hosting for your next website? Take advantage of exclusive parteners offers that we have secured just for you, & launch your site in seconds with <a href=""> #avada</a> on WP Engine hosting & get 30% Off this Black Friday <a href=""> bit.ly/3kjLLE2 #BlackFriday2020</a></p>
                                </div>
                                <img class="img-fluid" src="../assets/img/En2TRxLW4AEiWUN.jpeg" alt="WP Engigneer">
                                <div class="d-flex justify-content-between">
@@ -40,32 +40,79 @@ export default {
                             </div>
                         </div>
                     </div>
-                    <div class="col-3 border">
-                        <h4>Contact us today</h4>
+                    <div class="col-3 me-2">
+                        <h6 class="mb-3 fw-bolder">Contact us today</h6>
                         <div class="d-flex">
-                            <i class="fa-solid fa-globe mx-2"></i>
-                            <p class="fonts-10 fw-bolder lh-lg"> Corporate Location 1600 Amphitheatre Parkway London Wc1 1Ba</p>
+                            <div>
+                                <i class="fa-solid fa-globe mx-2 text-secondary"></i>
+                            </div>
+                            <div>
+                                <p class="fonts-9 fw-bolder lh-lg text-secondary"> Corporate Location <br> 1600 Amphitheatre Parkway <br> London WC1 1Ba</p>
+                            </div>
                         </div>
+                        <div class="d-flex">
+                            <div>
+                                <i class="fa-solid fa-house mx-2 text-secondary"></i>
+                            </div>
+                            <div>
+                                <p class="fonts-9 fw-bolder lh-lg text-secondary">Residential Location <br> 9521 broadsberry Avenue <br> Paddignton Rc7 gZA</p>
+                            </div>
+                        </div>
+                        <div class="d-flex">
+                            <div>
+                                <i class="fa-solid fa-phone mx-2 text-secondary"></i>
+                            </div>
+                            <div>
+                                <p class="fonts-9 fw-bolder lh-lg text-secondary"> 1.800.458.566 / 1800.532.2112</p>
+                            </div>
+                        </div>
+                        <div class="d-flex">
+                            <div>
+                                <i class="fa-solid fa-envelope mx-2 text-secondary"></i>
+                            </div>
+                            <div>
+                                <p class="fonts-9 fw-bolder lh-lg text-secondary">info@your-domani.com</p>
+                            </div>
+                        </div>
+                        <div class="d-flex">
+                            <div>
+                                <i class="fa-regular fa-clock mx-2 text-secondary"></i>
+                            </div>
+                            <div>
+                                <p class="fonts-9 fw-bolder lh-lg text-secondary"> Monday-Fryday 9:00 AM - 6:00 PM</p>
+                            </div>
+                        </div>
+                        <div class="d-flex">
+                            <div>
+                                <i class="fa-regular fa-clock mx-2 text-secondary"></i>
+                            </div>
+                            <div>
+                                <p class="fonts-9 fw-bolder lh-lg text-secondary"> Saturday - Sunday: 9:00 AM - 12:00 PM</p>
+                            </div>
+                        </div>   
                     </div>
-                    <div class="col-3 border">
-                        <h4>Find us</h4>
+                    <div class="col-3 me-2">
+                        <h6 class="mb-3 fw-bolder">Find Us</h6>
+                        <img class="img-fluid py-4" src="../assets/img/construction_map_pin.png-Notti" alt="">
                     </div>
                 </div>
-                
+    
             </div>
         </div>
-        <div class="container-full">
-            <div class="row border">
-                <div class="col-12 px-4">
-                    <div class="py-4 d-flex justify-content-between">
-                        <div>
-                            <h5 class="text-uppercase">copy-right 2012-2015 avada theme by theme fusion | all right reserved | powered by wordpress</h5>
-                        </div>
-                        <div class="d-flex">
-                            <a class="mx-2" href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                            <a class="mx-2" href="#"><i class="fa-brands fa-twitter"></i></a>
-                            <a class="mx-2" href="#"><i class="fa-brands fa-youtube"></i></a>
-                            <a class="mx-2" href="#"><i class="fa-brands fa-instagram"></i></a>
+        <div class="bg-color-black">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 px-4">
+                        <div class="py-4 d-flex justify-content-between">
+                            <div>
+                                <h5 class="text-uppercase">copy-right 2012-2015 avada theme by theme fusion | all right reserved | powered by wordpress</h5>
+                            </div>
+                            <div class="d-flex">
+                                <a class="mx-2" href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                                <a class="mx-2" href="#"><i class="fa-brands fa-twitter"></i></a>
+                                <a class="mx-2" href="#"><i class="fa-brands fa-youtube"></i></a>
+                                <a class="mx-2" href="#"><i class="fa-brands fa-instagram"></i></a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -77,17 +124,26 @@ export default {
 <style lang="scss" scoped>
 @use "../components/style/partials/vars" as *;
 
-.container-full:nth-child(2) {
+
+.bg-color-black{
     background-color: $black;
 }
 
-.bg-color{
-    background-color: $black;
+.bg-color-light-black{
+    background-color: $light_black;
+}
+
+.color-grey{
+    color: $grey;
 }
 
 h5 {
     font-size: 10px;
     color: $light_black;
+}
+
+h6{
+    color: $yellow;
 }
 
 p{
