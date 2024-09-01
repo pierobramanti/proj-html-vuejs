b9193ed19ab19e8646329de1a5d275856d7aea07
pierobramanti <piero.bramanti94@gmail.com>
2024-09-01 20:18:48 +0200

    [UPD] working on carousel.vue

--- src/components/partials/Carousel.vue
+++ src/components/partials/Carousel.vue
@@ -6,14 +6,18 @@ export default {
 
 <template>
     <div class="bg-carousel position-relative">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 d-flex flex-column justify-content-center align-items-center text-center h-100">
-                    <h1 class="text-white fs-1 fw-lighter">Our Owners Says</h1>
-                    <hr class="fw-bolder w-50">
+        <div class="container h-100 d-flex align-items-center justify-content-center">
+            <div class="row min-vh-100 d-flex justify-content-center align-items-center">
+                <div class="col-12 d-flex flex-column justify-content-center align-items-center text-center">
+                    <h1 class="text-white fs-1 fw-lighter mb-2">Our Home Owners Says</h1>
+                    <hr class="fw-bolder w-50 mb-4">
                     <img src="../../assets/img/home-testimonial-113165296.jpg" alt="" class="my-3 rounded-circle">
-                    <p class="text-white fw-medium lh-lg fst-italic">
-                        "No man but feels more of a man in the world if he has but a bit of ground that he can call his own. However small it is on the surface, it is four thousand miles deep. And that is a very handsome property."
+                    <div class="d-flex">
+                        <a href=""><i class="fa-solid fa-circle text-white me-2"></i></a>
+                        <a href=""><i class="fa-regular fa-circle text-white me-2"></i></a>
+                    </div>
+                    <p class="text-white fw-medium lh-lg fst-italic px-5">
+                        "No man but feels more of a man in the world if he has but a bit of ground <br> that he can call his own. However small it is on the surface, it is four thousand <br> miles deep. And that is a very handsome property."
                     </p>
                 </div>
             </div>
@@ -23,6 +27,10 @@ export default {
 
 <style lang="scss" scoped>
 @use "../style/partials/vars" as *;
+
+.color-yellow{
+    color: $yellow;
+}
 
 .bg-carousel {
     background-image: url("../../assets/img/home-parallax-144609983.jpg");
@@ -53,4 +61,12 @@ export default {
         border-radius: 100% 100% 0 0; 
     }
 }
+
+hr {
+    width: 30px;
+    color: $yellow;
+    border: solid 2px;
+}
+
 </style>
+
