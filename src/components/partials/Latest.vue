02e12b3782a133cc5a40d9fc80e29c11d29d16b1
pierobramanti <piero.bramanti94@gmail.com>
2024-09-01 19:07:17 +0200

    [UPD] static layout latest.vue with cards finished

--- src/components/partials/Latest.vue
+++ src/components/partials/Latest.vue
@@ -5,21 +5,49 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 text-center mb-4 mt-4">
-                <h3 class="fs-1 fw-lighter">Latest News</h3>
-                <hr class="fw-bolder mt-2 mb-4">
-                <h4 class="fonts-10 mt-2 mb-4 lh-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio distinctio reprehenderit quam iusto,<br>sint nulla omnis ex debitis nisi aspernatur possimus repellendus adipisci</h4>
-            </div>
-            <div class="col-12 d-flex">
-                <div class="custom-card">
-                    <img src="../../assets/img/blog-post-134132600-1200x723.jpg" class="card-img" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">Redeveloping Florida's Southern Cost</h4>
-                        <p class="card-date">December 7th, 2015</p>
-                        <hr class="card-separator">
-                        <p class="card-text">Technology is here to stay, Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda harum deserunt magnam aliquam labore tempora eum expedita corrupti ea dolores? Porro voluptatum a dignissimos iste similique ducimus eaque error explicabo!</p>
+    <div class="bg-grey">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center mb-4 mt-4">
+                    <h3 class="fs-1 fw-lighter">Latest News</h3>
+                    <hr class="fw-bolder mt-2 mb-4">
+                    <h4 class="fonts-10 mt-2 mb-4 lh-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio distinctio reprehenderit quam iusto,<br>sint nulla omnis ex debitis nisi aspernatur possimus repellendus adipisci</h4>
+                </div>
+                <div class="col-12 d-flex justify-content-center gap-4">
+                    <div class="custom-card">
+                        <img src="../../assets/img/blog-post-134132600-1200x723.jpg" class="card-img" alt="...">
+                        <div class="card-body bg-white">
+                            <h4 class="card-title">Redeveloping Florida's Southern Cost</h4>
+                            <p class="card-date">December 7th, 2015</p>
+                            <hr class="card-separator">
+                            <p class="card-text">Technology is here to stay, Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda harum deserunt magnam aliquam labore tempora eum expedita corrupti ea dolores? Porro voluptatum a dignissimos iste similique ducimus eaque error explicabo!</p>
+                        </div>
+                    </div>
+                    <div class="custom-card">
+                        <img src="../../assets/img/blog-post-92486644-1200x723.jpg" class="card-img" alt="...">
+                        <div class="card-body bg-white">
+                            <h4 class="card-title">How We manage Large construction Project</h4>
+                            <p class="card-date">December 7th, 2015</p>
+                            <hr class="card-separator">
+                            <p class="card-text">Technology is here to stay, Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda harum deserunt magnam aliquam labore tempora eum expedita corrupti ea dolores? Porro voluptatum a dignissimos iste similique ducimus eaque error explicabo!</p>
+                        </div>
+                    </div>
+                    <div class="custom-card">
+                        <img src="../../assets/img/blog-post-332773904-1200x723.jpg" class="card-img" alt="...">
+                        <div class="card-body bg-white">
+                            <h4 class="card-title">Future Proving a modern Home</h4>
+                            <p class="card-date">December 7th, 2015</p>
+                            <hr class="card-separator">
+                            <p class="card-text">Technology is here to stay, Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda harum deserunt magnam aliquam labore tempora eum expedita corrupti ea dolores? Porro voluptatum a dignissimos iste similique ducimus eaque error explicabo!</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Sezione con i due hr e l'H4 centrato -->
+                <div class="col-12 text-center mt-5 mb-5">
+                    <div class="d-flex align-items-center justify-content-center">
+                        <hr class="flex-grow-1 text-white">
+                        <h4 class="text-uppercase fs-5 text-secondary mx-3">view all articles</h4>
+                        <hr class="flex-grow-1 text-white">
                     </div>
                 </div>
             </div>
@@ -29,6 +57,14 @@ export default {
 
 <style lang="scss" scoped>
 @use "../style/partials/vars" as *;
+
+.bg-grey {
+    background-color: $grey;
+}
+
+.bg-white{
+    background-color: $white;
+}
 
 hr {
     color: $yellow;
@@ -57,7 +93,7 @@ hr {
 
 .card-title {
     font-size: 1.25rem;
-    color: #6c757d;
+    color: #464646;
     font-weight: 500;
     margin-bottom: 10px;
 }
@@ -76,7 +112,11 @@ hr {
 
 .card-text {
     font-size: 1rem;
-    color: #333;
+    color: #6b6868;
     text-align: left;
+}
+
+.text-white {
+    border-color: rgb(255, 252, 252) !important;
 }
 </style>
